3d853e1b32583d75f7c2c62f1eceb59c275721a3
MuhammadGhazy134 <5026241134@student.its.ac.id>
2025-12-16 19:41:54 +0700

    MazeGame push v1

---
+++ MazeGame/src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MazeFrame frame = new MazeFrame();
+            frame.setVisible(true);
+        });
+    }
+
+}
